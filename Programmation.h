77d1c4d9f9a3ac8af0aa2ad5a62694f8ebb0faf5
Yosri <yosri.jedidi@esprit.tn>
2020-12-24 10:35:52 +0100

    MAJ VALEUR AJOUTEE

---
+++ Programmation.h
@@ -0,0 +1,58 @@
+#ifndef PROGRAMMATION_H
+#define PROGRAMMATION_H
+#include "film.h"
+#include "ticket.h"
+#include "client.h"
+#include "server.h"
+#include "notification.h"
+#include <QDialog>
+#include <QSound>
+namespace Ui {
+class Programmation;
+}
+
+class Programmation : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Programmation(QWidget *parent = nullptr);
+    ~Programmation();
+
+private slots:
+
+
+    void on_pushButton_ajoutTicket_clicked();
+
+    void on_pushButton_supprimerTicket_clicked();
+
+    void on_pushButton_ajoutFilm_clicked();
+
+    void on_pushButton_suppFilm_clicked();
+
+    void on_pushButton_modifTicket_clicked();
+
+    void on_pushButton_modifFilm_clicked();
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_radioButton_clicked();
+
+    void on_tableView_Film_activated(const QModelIndex &index);
+
+    void on_pushButton_rechercheF_clicked();
+
+    void on_pushButton_Chat_clicked();
+
+private:
+    Ui::Programmation *ui;
+    Film tempFilm;
+    Ticket tempTicket;
+    Server *serverdialog;
+    Client *clientdialog;
+    notification *mysystem;
+};
+
+#endif // PROGRAMMATION_H
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include <QDebug>
+#include <QApplication>
+#include <QMessageBox>
+#include "connexion.h"
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Connexion C;
+    bool test=C.ouvrirConnexion();
+    MainWindow w;
+    if (test)
+    { w.show();
+        QMessageBox::information(nullptr,QObject::tr("Database is open"),QObject::tr("connection succesful \n click cancel to exit")
+                ,QMessageBox::Cancel);
+    }
+              else
+                  QMessageBox::critical(nullptr,QObject::tr("Database is not open"),QObject::tr("connection failed \n click cancel to exit")
+                          ,QMessageBox::Cancel);
+ return a.exec();
+}
